6ced9ed2bd40b07429badf8cd0e442d5b461d58b
Sameh Mahrous <MEHA@Samehs-MacBook-Pro.local>
2022-03-06 17:56:39 -0500

    Homework 2

---
+++ src/Homework/Homework2.java
@@ -0,0 +1,29 @@
+package Homework;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        byte numberOfHoursInDay = 24;
+            System.out.println("Number of hours in a day = " + numberOfHoursInDay + " Hours.");
+        short numberOfDaysInYear = 365;
+            System.out.println("Number of days in one year = " + numberOfDaysInYear + " Days.");
+        int numberOfEmployeesInAnOrganization = 2000;
+            System.out.println("Number of employees in an organization = " + numberOfEmployeesInAnOrganization + " Employees.");
+        long numberOfPopulationInUsa = 332403650;
+            System.out.println("Number of population in USA = " + numberOfPopulationInUsa + " People.");
+        float mortgageRate = 4.365f;
+            System.out.println("Mortgage Rate for 2022 = " + mortgageRate + "%.");
+        double balanceInBankAccount = 200.91;
+            System.out.println("Your Account Balance is = " + "$" + balanceInBankAccount);
+        boolean sunRise = false;
+            System.out.println("Does the sun rise from the West? " + sunRise);
+        char myNameInitial1 = 'S';
+        char myNameInitial2 = 'M';
+            System.out.println("My name initials are: " + myNameInitial1 + myNameInitial2);
+        String myName = "Sam Mahrous.";
+        System.out.println("My Name is: " + myName);
+
+
+
+
+    }
+}
